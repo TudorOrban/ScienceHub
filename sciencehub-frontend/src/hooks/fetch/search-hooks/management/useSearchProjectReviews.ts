241c92562ea19e3c30ca9286c28544a2ab3e1b89
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-09-02 10:19:33 +0300

    Add React Query hook wrapper of Search Project Reviews service

--- sciencehub-frontend/src/hooks/fetch/search-hooks/management/useSearchProjectReviews.ts
+++ sciencehub-frontend/src/hooks/fetch/search-hooks/management/useSearchProjectReviews.ts
@@ -1,7 +1,8 @@
 import { searchProjectReviews } from "@/src/services/fetch/reviews/searchProjectReviews";
 import { ProjectReviewSearchDTO } from "@/src/types/managementTypes";
-import { PaginatedResults, Result } from "@/src/types/searchTypes";
+import { PaginatedResults, Result, StdError } from "@/src/types/searchTypes";
 import { SmallSearchOptionsNew } from "@/src/types/utilsTypes";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 
@@ -37,4 +38,41 @@ export const useSearchProjectReviews = (options: SmallSearchOptionsNew) => {
     }, [options.entityId, options.enabled, options.page, options.itemsPerPage, options.searchQuery, options.sortBy, options.sortDescending]);
 
     return result;
+};
+
+
+export const useSearchProjectReviewsRQ = (options: SmallSearchOptionsNew) => {
+    const queryKey = ['projectReviews', options];
+  
+    const fetchProjectReviews = async () => {
+        const response = await searchProjectReviews(options);
+        if (response.error) {
+          throw response; 
+        }
+        return response.data;
+    };
+  
+    const { data, error, isLoading } = useQuery({
+        queryKey,
+        queryFn: fetchProjectReviews,
+        enabled: options.enabled,
+        keepPreviousData: true,
+        staleTime: 5000,
+        cacheTime: 30000,
+        onError: (error) => {
+          console.error('Error fetching project reviews:', error);
+        }
+      });
+    
+    const transformedError: StdError | undefined = (error && typeof error === 'object' && 'title' in error && 'message' in error) ? {
+        title: (error as StdError).title,
+        message: (error as StdError).message,
+        code: (error as StdError).code
+    } : undefined;
+    
+    return {
+        data: data || { results: [], totalCount: 0 },
+        error: transformedError,
+        isLoading
+    };
 };
